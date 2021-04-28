192d23e1b469ea06331ca0eeab4a9e3b48607ea6
Catherine Dean <catherinemdean15@gmail.com>
2021-04-28 09:49:31 -0600

    Create remaining_vacation_days method and test for Worker model

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -2,4 +2,15 @@ class Worker < ApplicationRecord
   has_many :requests
   has_many :managers_workers
   has_many :managers, through: :managers_workers
+
+  def remaining_vacation_days
+    requests = self.requests.where(status: 'approved')
+    total_days_approved = 0
+    requests.each do |request|
+      # subtract the beginning date from the end date and divide by number of seconds in a day
+      total_days_approved += (request.vacation_end_date - request.vacation_start_date) / 86_400
+    end
+    # this assumes requests will be archived at the beginning of every year
+    30 - total_days_approved
+  end
 end
--- spec/models/worker_spec.rb
+++ spec/models/worker_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Worker, type: :model do
     it { should have_many :managers_workers }
     it { should have_many(:managers).through(:managers_workers) }
   end
+
+  describe 'instance methods' do
+    it "shows a worker's remaining vacation days" do
+      worker = Worker.create!(name: 'Worker 1')
+      Request.create!(status: 0,
+                      vacation_start_date: '2021-07-24T00:00:00.000Z',
+                      vacation_end_date: '2021-08-04T00:00:00.000Z',
+                      worker_id: worker.id)
+      Request.create!(status: 0,
+                      vacation_start_date: '2021-08-24T00:00:00.000Z',
+                      vacation_end_date: '2021-09-04T00:00:00.000Z',
+                      worker_id: worker.id)
+      Request.create!(status: 2,
+                      vacation_start_date: '2021-09-24T00:00:00.000Z',
+                      vacation_end_date: '2021-10-04T00:00:00.000Z',
+                      worker_id: worker.id)
+
+      expect(worker.remaining_vacation_days).to eq(8)
+    end
+  end
 end
